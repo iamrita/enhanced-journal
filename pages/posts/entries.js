ea20e08f96bf81e6ac94a43135b5aea4276d5fd7
Amrita <amritav.ca@gmail.com>
2023-04-03 18:14:05 -0700

    gets and receives data from firebase

--- pages/posts/entries.js
+++ pages/posts/entries.js
@@ -1,13 +1,33 @@
-import Head from 'next/head';
+import Head from "next/head";
+import firebase from "../../firebase";
+import { useEffect, useState } from "react";
+
 
 export default function Entries() {
-    return (
-        <div>
+  const [entry, setEntry] = useState({});
 
-            <Head>
-                <title>Entries</title>
-            </Head>
-            <h3>Entries</h3>
-        </div>
-    )
+  useEffect(() => {
+    const database = firebase.database();
+    let ref = database.ref("journalEntry");
+  
+    ref.on("value", (snapshot) => {
+      const data = snapshot.val();
+      setEntry({ date: snapshot.val().date, entry: snapshot.val().entry });
+      console.log(data);
+    });
+
+  }, [])
+
+
+  return (
+    <div>
+      <Head>
+        <title>Entries</title>
+      </Head>
+      <h3>Entries</h3>
+      <h2>Date: {entry.date}</h2>
+      <h2>Entry: </h2>
+      <p>{entry.entry}</p>
+    </div>
+  );
 }
