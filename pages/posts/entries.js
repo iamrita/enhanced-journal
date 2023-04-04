d43a69b943761f691b81cae5e65e483155f06e87
Amrita <amritav.ca@gmail.com>
2023-04-04 13:42:57 -0700

    entries are now saved and showing up on the entries page

--- pages/posts/entries.js
+++ pages/posts/entries.js
@@ -1,23 +1,21 @@
 import Head from "next/head";
 import firebase from "../../firebase";
 import { useEffect, useState } from "react";
-import styles from "../index.module.css"
+import styles from "../index.module.css";
 import Link from "next/link";
 
-
 export default function Entries() {
-  const [entry, setEntry] = useState({});
+  const [entries, setEntries] = useState([]);
 
   useEffect(() => {
     const database = firebase.database();
     let ref = database.ref("journal-entries");
 
-    ref.once("value").then((snapshot) => {
-      snapshot.forEach((childSnapshot) => {
-        const data = childSnapshot.val();
-        setEntry({ date: data.date, entry: data.entry });
-        console.log(data);
-      });
+    ref.on("value", (snapshot) => {
+      const data = snapshot.val();
+      const childValues = Object.values(data);
+      setEntries(childValues);
+      console.log(childValues);
     });
   }, []);
 
@@ -28,9 +26,13 @@ export default function Entries() {
       </Head>
       <main className={styles.main}>
         <h3>Entries</h3>
-        <h2>Date: {entry.date}</h2>
-        <h2>Entry: </h2>
-        <p>{entry.entry}</p>
+        {entries.reverse().map((entry) => (
+          <div>
+            <h2>Date: {entry.date}</h2>
+            <p><b>Entry:</b> {entry.entry}</p>
+          </div>
+        ))}
+
         <Link href="/"> Back To Home </Link>
       </main>
     </div>
